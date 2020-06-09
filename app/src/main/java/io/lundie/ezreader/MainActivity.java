287fd5c01a74d604a40ad9d8b43dd87ad3a848cf
Michael Lundie <michael@tal.tokyo>
2020-06-09 20:41:41 +0900

    Basic Project Set-Up

---
+++ app/src/main/java/io/lundie/ezreader/MainActivity.java
@@ -0,0 +1,16 @@
+package io.lundie.ezreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.lundie.ezreader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
